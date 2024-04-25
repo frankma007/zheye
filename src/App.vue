c12a96cebab2ca219048a8759542bc4ccb141e61
frankma007 <1015630728@qq.com>
2024-04-25 13:14:16 +0800

    typescript版本不能太高，4.3.5就好，不然会报些莫名其妙的错误

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,55 @@
 <template>
-
+<!--
 <img alt="Vue logo" src="./assets/logo.png" />
-<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-<ColumnList :list="list"></ColumnList>
+<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+<GlobalHeader :user="user"></GlobalHeader>
+<!-- <ColumnList :list="list"></ColumnList> -->
 </template>
 
 <script lang="ts">
 import {
   defineComponent, ref
 } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
-const testData:ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1专栏',
-    avatar: '/img/logo.png',
-    description: '简介，我是简介'
-  },
-  {
-    id: 2,
-    title: 'test2专栏',
-    // avatar: 'https://cli.vuejs.org/favicon.png',
-    description: '简介，我是简介'
-  },
-  {
-    id: 3,
-    title: 'test3专栏',
-    avatar: 'https://cli.vuejs.org/favicon.png',
-    description: '简介，我是简介'
-  },
-  {
-    id: 4,
-    title: 'test4专栏',
-    avatar: 'https://cli.vuejs.org/favicon.png',
-    description: '简介，我是简介444'
-  }
-]
+// import HelloWorld from './components/HelloWorld.vue'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const testUser:UserProps = {
+  isLogin: true,
+  name: '爱面子',
+  id: 434
+}
+// const testData:ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1专栏',
+//     avatar: '/img/logo.png',
+//     description: '简介，我是简介'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2专栏',
+//     // avatar: 'https://cli.vuejs.org/favicon.png',
+//     description: '简介，我是简介'
+//   },
+//   {
+//     id: 3,
+//     title: 'test3专栏',
+//     avatar: 'https://cli.vuejs.org/favicon.png',
+//     description: '简介，我是简介'
+//   },
+//   {
+//     id: 4,
+//     title: 'test4专栏',
+//     avatar: 'https://cli.vuejs.org/favicon.png',
+//     description: '简介，我是简介444'
+//   }
+// ]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld, ColumnList
+    // ColumnList,
+    GlobalHeader
   },
   setup () {
     /* const arr = ref([
@@ -54,7 +62,8 @@ export default defineComponent({
 
     ]) */
     return {
-      list: testData
+      // list: testData,
+      user: testUser
     }
   }
 })
