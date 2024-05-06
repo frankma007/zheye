0d4e3197994335a28eda090b469eb614f8fd2847
frankma007 <1015630728@qq.com>
2024-05-06 18:03:05 +0800

    222

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
-
+import axios from 'axios'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+axios.get('/columns').then(resp => {
+  console.log(resp, '输出')
+})
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params, icode: 'FB4C6B8434F751FA' }
+  return config
+})
 
 const app = createApp(App)
 app.use(router)
